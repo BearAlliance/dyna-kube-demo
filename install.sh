982401183d24a56d73632c44e06256d9cbfa6b4b
Nick Cacace <NickCacace@gmail.com>
2020-11-24 16:12:15 -0500

    remove now unused install script

--- install.sh
+++
@@ -1,7 +0,0 @@
-echo "Installing frontend dependencies"
-npm install --prefix ./frontend
-
-echo "Installing presentation layer dependencies"
-npm install --prefix ./presentation-layer
-
-echo "Done!"
